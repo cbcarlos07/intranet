ccf4df05b72b1b89d2f7b3f97ed9d3e6d64e98f9
cbcarlos07 <cbcarlos08@gmail.com>
2016-07-22 14:24:36 -0400

    .

--- index.php
+++ index.php
@@ -13,26 +13,13 @@
 <div class="container">
     
     
-    <div class="col-md-push-5 " id="principal">
+    <div class="col-md-push-5 " >
          
 	 <!------- 1 linha de icones -------->
          <center>
 	 <div class="row cont" >
              	              
-             <div class="span3 quadro">
-                    <a href="links/mv">
-                    <div class="imagem">
-                        <img src="img/lista.png" class="img_quadro" style="width: 76%; margin-top: 7%;">
-                    </div>
-            	
-                    <div class="conteudo">
-
-                        <font face="Arial">SOULMV</font>
-                        <p>Gestão Hospitalar</p>
-                        <!--<p style="margin-top:10px" class="fonte">SOULMV</p>-->
-                    </div>
-                          </a>
-		</div>
+             
         
 		<div class="span3 quadro">
                     <a href="links/mv">
@@ -96,7 +83,17 @@
         </a>
 		
 		
-		  
+	<a href="http://srvsmvsge001/Painel/">
+                <div class="span3 quadro">
+                        <div class="imagem">
+                                <img src="img/indicadores.png" class="img_quadro" style="width:78%">
+                        </div>
+                        <div class="conteudo">
+                            <font face="Arial" style="margin:-20px 0px 0px 30px;">PAINEL</font>
+                            <p>Painel de Indicadores</p>
+                        </div>
+                </div>
+        </a>
         
         
         
@@ -109,24 +106,29 @@
 	
     <div class="row cont" align="center">
         
-         <a href="http://srvsmvsge001/Painel/">
-            <div class="span3 quadro">
+        <div class="span3 quadro">
+                    <a href="http://soulmvlinks/ramais">
                     <div class="imagem">
-                            <img src="img/indicadores.png" class="img_quadro" style="width:78%">
+                        <img src="img/lista.png" class="img_quadro" style="margin:19px 0px 0px 10px;">
                     </div>
+            	
                     <div class="conteudo">
-                        <font face="Arial" style="margin:-20px 0px 0px 30px;">PAINEL</font>
-                        <p>Painel de Indicadores</p>
+
+                        <font face="Arial" style="margin:-20px 0px 0px 25px;">RAMAIS</font>
+                        <p>Lista de Ramais</p>
+                        <!--<p style="margin-top:10px" class="fonte">SOULMV</p>-->
                     </div>
-            </div>
-        </a>
+                          </a>
+	</div>
+        
+         
 	       <a href="links/microdata">
 				<div class="span3 quadro">
 					<div class="imagem">
-						<img src="img/microdata.png" class="img_quadro" style="width:76%;margin-top: 50px;">
+						<img src="img/microdata.png" class="img_quadro" style="width:76%;margin-top: 40px;">
 					</div>
 					<div class="conteudo">
-						<font face="Arial" style="margin:-5px 0px 0px 0px;">MICRODATA</font>
+						<font face="Arial" style="margin:-20px 0px 0px 0px;">MICRODATA</font>
 						<p>Diagnóstico por Imagem</p>
 					</div>
 				</div>
@@ -138,7 +140,7 @@
 						<img src="img/mconnect.png" class="img_quadro" style="width:76%;margin-top: 50px;">
 					</div>
 					<div class="conteudo">
-						<font face="Arial" style="margin:-5px 0px 0px 0px;">MCONNECT</font>
+						<font face="Arial" style="margin:-20px 0px 0px 0px;">MCONNECT</font>
 						<p>Visualizador de Imagem</p>
 					</div>
 				</div>
@@ -275,6 +277,7 @@
 </div>
 
 <script type="text/javascript">
+//function atualizaContador(YY,MM,DD,HH,MI,dia, hora, minuto, segundo) {
 function atualizaContador(YY,MM,DD,HH,MI,saida) {
 	var SS = 00;
 	var hoje = new Date();
@@ -290,21 +293,44 @@ function atualizaContador(YY,MM,DD,HH,MI,saida) {
 	hh = hh - (dd * 24);
 
 	var faltam = '';
-	faltam +=  dd+'    :   &nbsp;' ;
-	faltam += (toString(hh).length) + hh+'      :         ';
-	faltam += (toString(mm).length) + mm+'      :       ';
+        var horas = '';
+        var minutos = '';
+        var dias = '';
+        //dia = dd;
+        //hora = hh;
+       // minuto = mm;
+       // segundo = ss;
+	faltam +=  dd+'&nbsp;&nbsp;&nbsp;' ;
+	faltam += hh < 10 ? '0'+(toString(hh).length)+'&nbsp;&nbsp;' : hh+'&nbsp;&nbsp;';
+	faltam += mm < 10 ? '0'+(toString(mm).length)+'&nbsp;&nbsp;' : mm+'&nbsp;&nbsp;&nbsp;';
+        //dias = dd < 10 ? '0'+dd : dd;
+        //horas = hh < 10 ? '0'+hh : hh;
+        //minutos = mm < 10 ? '0'+mm : mm;
 	faltam += ss < 10 ? '0'+ss : ss;
 	if (dd+hh+mm+ss > 0) {
-		document.getElementById(saida).innerHTML = faltam;
+		//document.getElementById(dia).innerHTML = dias;
+                //document.getElementById(hora).innerHTML = horas;
+                //document.getElementById(minuto).innerHTML = minutos;
+                
+               // document.getElementById(segundo).innerHTML = faltam;
+                document.getElementById(saida).innerHTML = faltam;
 		setTimeout(function(){atualizaContador(YY,MM,DD,HH,MI,saida)},1000);
+                //setTimeout(function(){atualizaContador(YY,MM,DD,HH,MI,dia,hora,minuto,segundo)},1000);
+                
 	} else {
-		document.getElementById(saida).innerHTML = 'ESTAMOS EM VISITA';
+                document.getElementById(saida).innerHTML = 'ESTAMOS EM VISITA';
+		//document.getElementById(dia).innerHTML = 'ESTAMOS EM VISITA';
+                //document.getElementById(hora).innerHTML = '';
+                //document.getElementById(minuto).innerHTML = '';
+                //document.getElementById(segundo).innerHTML = '';
 		setTimeout(function(){atualizaContador(YY,MM,DD,HH,MI,saida)},1000);
+                //setTimeout(function(){atualizaContador(YY,MM,DD,HH,MI,dia,hora,minuto,segundo)},1000);
 	}
 }
 
 window.onload=function(){
-	atualizaContador('2016','10','06','09','00','elemento');
+	//atualizaContador('2016','10','06','09','00','dia','hora','minuto','segundo');
+        atualizaContador('2016','10','06','09','00','cronometro');
 }
 </script>
 
